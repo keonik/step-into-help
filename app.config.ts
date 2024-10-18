6495bd33c1f5f98f956dba15184670e414b1c84d
John Fay <jfay@miletwo.us>
2024-10-18 14:04:18 -0400

    adjust build based on documentation

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,9 @@
 // app.config.ts
 import { defineConfig } from "@tanstack/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  // https://tanstack.com/router/latest/docs/framework/react/start/hosting/#nodejs
+  server: {
+    preset: "node-server",
+  },
+});
